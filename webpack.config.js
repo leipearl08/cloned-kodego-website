05fc7de51b3ece78aefb70b10930902011159759
Pearl Natata <leireen.natata@gmail.com>
2022-12-16 19:24:30 +0800

    Update markup of mobile nav trigger

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,39 @@
 const path = require('path')
 
 const postCSSPlugins = [
-require("postcss-import"),
-require("postcss-mixins"),
-require("postcss-simple-vars"),
-require("postcss-nested"),
-require("autoprefixer")
+  require("postcss-import"),
+  require("postcss-mixins"),
+  require("postcss-simple-vars"),
+  require("postcss-nested"),
+  require("autoprefixer")
 ]
 
 module.exports = {
-entry: "./app/assets/scripts/App.js",
-output: {
+  entry: "./app/assets/scripts/App.js",
+  output: {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
-},
-devServer: {
+  },
+  devServer: {
     before: function (app, server) {
-    server._watch('./app/**/*.html')
+      server._watch('./app/**/*.html')
     },
     contentBase: path.join(__dirname, "app"),
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-},
-mode: "development",
-module: {
+  },
+  mode: "development",
+  module: {
     rules: [
-    {
+      {
         test: /\.css$/i,
         use: ["style-loader", "css-loader?url=false", {
-        loader: "postcss-loader", options: {
+          loader: "postcss-loader", options: {
             postcssOptions: { plugins: postCSSPlugins }
-                        }
-                    }
-                ]
-            }
-        ]
-    }
+          }
+        }]
+      }
+    ]
+  }
 }
-
